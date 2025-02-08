982ebab729849912d424b62ed64725f408786ec7
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2025-02-08 15:44:15 +0700

    create_app_bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,81 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1'
+
+const MENU_STYLE = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box sx={{
       height: (theme) => theme.trello.boardBarHeight,
       width: 'full',
-      backgroundColor: 'primary.dark',
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      borderTop: '1px solid #00bfa5',
+      gap: 2,
+      overflowX: 'auto',
+      p: 2
     }}>
-        Board Content
+      <Box sx={{ display:'flex', alignContent: 'center', gap: 2 }}>
+        <Chip sx={MENU_STYLE} icon={<SpaceDashboardIcon />} label="Trello Dashboard" clickable/>
+        <Chip sx={MENU_STYLE} icon={<VpnLockIcon/>} label="Public/Private Workspace" clickable/>
+        <Chip sx={MENU_STYLE} icon={<AddToDriveIcon />} label="Add to Google Drive" clickable/>
+        <Chip sx={MENU_STYLE} icon={<BoltIcon />} label="Automation" clickable/>
+        <Chip sx={MENU_STYLE} icon={<FilterListIcon />} label="Filter" clickable/>
+      </Box>
+      <Box sx={{ display:'flex', alignContent: 'center', gap: 2 }}>
+        <Button startIcon={<PersonAddAlt1Icon />} variant="outlined">Invited</Button>
+        <AvatarGroup max={4} sx ={{ 
+          '& .MuiAvatar-root': {
+            height: '34px',
+            width:'34px',
+            fontSize: '16px'
+          }
+        }}>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="nguyenngocbao">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
